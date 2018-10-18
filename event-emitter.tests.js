d5147219a281b59fa3309ca21253598b613cc9f6
Arnelle Balane <arnellebalane@gmail.com>
2018-10-18 17:51:31 +0800

    Add unit tests for emit method

--- event-emitter.tests.js
+++ event-emitter.tests.js
@@ -1,4 +1,5 @@
 import {describe} from 'riteway';
+import sinon from 'sinon';
 import EventEmitter from './event-emitter';
 
 describe('EventEmitter#constructor', async assert => {
@@ -73,5 +74,42 @@ describe('EventEmitter#off', async assert => {
 });
 
 describe('EventEmitter#emit', async assert => {
+    const instance = new EventEmitter();
 
+    const callback1 = sinon.spy();
+    const callback2 = sinon.spy();
+    instance.on('name1', callback1);
+    instance.on('name2', callback2);
+
+    instance.emit('name1', 'data1');
+
+    assert({
+        given: 'an event name and data',
+        should: 'call the callback functions for the event',
+        actual: callback1.calledOnce,
+        expected: true
+    });
+
+    assert({
+        given: 'an event name and data',
+        should: 'not call the callback functions for other events',
+        actual: callback2.notCalled,
+        expected: true
+    });
+
+    assert({
+        given: 'an event name and data',
+        should: 'pass the data to the callback functions for the event',
+        actual: callback1.calledOnceWithExactly('data1'),
+        expected: true
+    });
+
+    instance.emit('name2');
+
+    assert({
+        given: 'an event name and no data',
+        should: 'not pass anything to the callback functions for the event',
+        actual: callback2.calledOnceWithExactly(),
+        expected: true
+    });
 });
